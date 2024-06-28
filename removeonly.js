ae96ddba9b26d314a8433fe69985ef33d4a61874
TM0088 <73647362+TM0088@users.noreply.github.com>
2024-06-28 22:36:07 +0800

    onwheel

--- removeonly.js
+++ removeonly.js
@@ -2,7 +2,7 @@
 	function onMouseOver(event) {
 		var elem = event.target || event.srcElement;
 		elem.style.outline = '2px solid red';
-		prevEl = elem;
+		selectedElement = elem;
 	}
 
 	function onMouseOut(event) {
@@ -17,21 +17,38 @@
 
 	function onKeydown(event) {
 		if (event.key === "F8") {
-			prevEl.style.outline = '';
-            		cleanupEventListeners();
+			selectedElement.style.outline = '';
+            cleanupEventListeners();
 		} else if (event.key === "Escape") {
-            		prevEl.style.display = 'none';
+            selectedElement.style.display = 'none';
         }
 	}
-    
+
+    function onWheel(event) {
+        selectedElement.style.outline = '';
+        event.preventDefault();
+        if (event.deltaY < 0) {
+            selectedElement = selectedElement.parentElement;
+            selectedElement.style.outline = '2px solid red';
+        } else {
+            var childElement = selectedElement.querySelector(":hover");
+            if (childElement) {
+                selectedElement = childElement;
+                selectedElement.style.outline = '2px solid red';
+            }
+        }
+    }
+
     function cleanupEventListeners() {
         document.removeEventListener("mouseover", onMouseOver);
         document.removeEventListener("mouseout", onMouseOut);
         document.removeEventListener("click", onClick);
+        document.removeEventListener("wheel", onWheel);
     }
 
     document.addEventListener("mouseover", onMouseOver);
     document.addEventListener("mouseout", onMouseOut);
     document.addEventListener("click", onClick);
     document.addEventListener("keydown", onKeydown);
+    document.addEventListener("wheel", onWheel, { passive: false });
 })();
