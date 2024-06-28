002ed70786ce33c1bf85e4bbeac63db7b656adac
TM0088 <73647362+TM0088@users.noreply.github.com>
2024-06-28 22:43:20 +0800

    Update removeonly.js

--- removeonly.js
+++ removeonly.js
@@ -2,7 +2,7 @@
 	function onMouseOver(event) {
 		var elem = event.target || event.srcElement;
 		elem.style.outline = '2px solid red';
-		selectedElement = elem;
+		s = elem;
 	}
 
 	function onMouseOut(event) {
@@ -10,31 +10,26 @@
 		elem.style.outline = '';
 	}
 
-	function onClick(event) {
-		var elem = event.target || event.srcElement;
-		elem.style.display = 'none';
-	}
-
 	function onKeydown(event) {
 		if (event.key === "F8") {
-			selectedElement.style.outline = '';
+			s.style.outline = '';
             cleanupEventListeners();
 		} else if (event.key === "Escape") {
-            selectedElement.style.display = 'none';
+            s.style.display = 'none';
         }
 	}
 
     function onWheel(event) {
-        selectedElement.style.outline = '';
+        s.style.outline = '';
         event.preventDefault();
         if (event.deltaY < 0) {
-            selectedElement = selectedElement.parentElement;
-            selectedElement.style.outline = '2px solid red';
+            s = s.parentElement;
+            s.style.outline = '2px solid red';
         } else {
-            var childElement = selectedElement.querySelector(":hover");
+            var childElement = s.querySelector(":hover");
             if (childElement) {
-                selectedElement = childElement;
-                selectedElement.style.outline = '2px solid red';
+                s = childElement;
+                s.style.outline = '2px solid red';
             }
         }
     }
@@ -42,13 +37,11 @@
     function cleanupEventListeners() {
         document.removeEventListener("mouseover", onMouseOver);
         document.removeEventListener("mouseout", onMouseOut);
-        document.removeEventListener("click", onClick);
         document.removeEventListener("wheel", onWheel);
     }
 
     document.addEventListener("mouseover", onMouseOver);
     document.addEventListener("mouseout", onMouseOut);
-    document.addEventListener("click", onClick);
     document.addEventListener("keydown", onKeydown);
     document.addEventListener("wheel", onWheel, { passive: false });
 })();
